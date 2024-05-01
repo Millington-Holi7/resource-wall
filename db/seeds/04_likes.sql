5226c74eb805460152dc071d22c489b9a31d1a7d
Millington-Holi7 <holinas007@gmail.com>
2024-05-01 11:47:37 -0400

    update table name

--- db/seeds/04_likes.sql
+++ db/seeds/04_likes.sql
@@ -1,8 +1,8 @@
-INSERT INTO post_comments (id, user_id, post_id) VALUES (
+INSERT INTO post_likes(id, user_id, post_id) VALUES (
 1, 1, 2);
-INSERT INTO post_comments (id, user_id, post_id) VALUES (
+INSERT INTO post_likes(id, user_id, post_id) VALUES (
 2, 4, 1);
-INSERT INTO post_comments (id, user_id, post_id) VALUES (
+INSERT INTO post_likes(id, user_id, post_id) VALUES (
 3, 3, 7);
-INSERT INTO post_comments (id, user_id, post_id) VALUES (
+INSERT INTO post_likes(id, user_id, post_id) VALUES (
 4, 5, 6);
